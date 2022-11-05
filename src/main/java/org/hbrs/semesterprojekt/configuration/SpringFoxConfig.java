388277163369fd37ab7999e9835f53edd9d27fd6
David Arambasic <arambasicdavid@yahoo.de>
2022-11-05 12:00:02 +0100

    Enabled Swagger 2 to our application

--- src/main/java/org/hbrs/semesterprojekt/configuration/SpringFoxConfig.java
+++ src/main/java/org/hbrs/semesterprojekt/configuration/SpringFoxConfig.java
@@ -6,17 +6,17 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("org.hbrs.semesterprojekt"))
+                    .paths(PathSelectors.any())
+                    .build();
     }
 }
